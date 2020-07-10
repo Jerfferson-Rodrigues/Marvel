225c4310b06b83749c620e11a5f71b8cf134270f
Jerfferson-Rodrigues <67125687+Jerfferson-Rodrigues@users.noreply.github.com>
2020-07-09 21:20:09 -0300

    system api

---
+++ Arquivos/main.js
@@ -0,0 +1,30 @@
+const marvel = {
+
+    render: () =>{
+
+        const Apimarvel = 'https://gateway.marvel.com:443/v1/public/characters?ts=1594234020&apikey=b7f748725ac95088fab844a68ff91d5f&hash=fd5249736c176578ac59480b0e5fe424&limit=16';
+        const container = document.querySelector('#hero');
+        let contentHTML = '';
+
+        fetch(Apimarvel)
+
+            .then(res => res.json())
+            .then((json) => {
+                for (const hero of json.data.results) {
+                    let urlHero = hero.urls[0].url;
+                    contentHTML += `
+                    <div class="personagem">
+                        <a href="${urlHero}" target="_blank">
+                            <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img">
+                        </a>
+                        <h3 class="title">${hero.name}</h3>
+                     </div>`;
+                }
+
+                container.innerHTML= contentHTML;
+
+            })
+    }
+    
+}   
+marvel.render();
